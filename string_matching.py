c4dc024ce8f96d660a34fac43917950c26644285
JiaJianLee <louisleejj@gmail.com>
2021-03-12 10:56:11 +0100

    Minor fix

--- string_matching.py
+++ string_matching.py
@@ -27,21 +27,20 @@ def naive(pattern, source):
             return i
 
 
-def lps_table(pattern, lps):
+def lps_table(pattern, pattern_length):
     """Facilitate the Knuth-Morris-Pratt-Algorithm. LPS stands for Longest proper Prefix which is Suffix.
 
         Args:
             pattern (str): word to look for
-            lps ():
+            pattern_length (int): length of the word
 
         Returns:
             int: index of the word sought
         """
 
     longest_pre_suffix = 0
-    pattern_length = len(pattern)  # see naive, redundant?
     pointer = 1  # at index 0, lps always 0, so starts at 1
-    lps[0] = 0
+    lps = [0] * pattern_length
 
     while pointer < pattern_length:
         if pattern[longest_pre_suffix] == pattern[pointer]:
@@ -56,6 +55,8 @@ def lps_table(pattern, lps):
         else:
             longest_pre_suffix = lps[longest_pre_suffix - 1]
 
+    return lps
+
 
 def kmp(pattern, source):
     """Uses the Knuth-Morris-Pratt-Algorithm to search for a given pattern.
@@ -68,13 +69,13 @@ def kmp(pattern, source):
         int: index of the word sought
     """
 
-    lps_table(pattern, lps)  # Initialize
     pattern_length = len(pattern)
     source_length = len(source)
-    lps[0] = 0 * pattern_length
+    lps = lps_table(pattern, pattern_length)  # Initialize
 
     source_pointer = 0
     pattern_pointer = 0
+
     while source_pointer < source_length:
         # Character matches
         if pattern[pattern_pointer] == source[source_pointer]:
@@ -83,19 +84,20 @@ def kmp(pattern, source):
         # Character does not match
         else:
             if pattern_pointer != 0:
-                pattern_pointer = lps[pattern_pointer-1]
+                pattern_pointer = lps[pattern_pointer - 1]
             else:
                 source_pointer += 1
         # Pattern found and matched
         if pattern_pointer == pattern_length:
-            print(source_pointer - pattern_pointer)
-            pattern_pointer = lps[pattern_pointer - 1]
+            return source_pointer - pattern_pointer
+            
 
 
 def main():
     # my_parser = argparse.ArgumentParser(description='Match the index of a target word')
     # my_parser.add_argument('Word', metavar='word', type=str, help='the word to search for')
-    print(naive("GJ", "AAAABBGGJJJJ"))  # test
+    print(kmp("GJ", "AAAABBGGJJJJ"))  # test
+    print(kmp("onion", "onisonionskl"))  # test
 
 
 if __name__ == '__main__':
